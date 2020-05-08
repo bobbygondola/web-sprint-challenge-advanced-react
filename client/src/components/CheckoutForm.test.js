94031adf27b42a4d9bb2354a9831fa0e542e2758
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-05-08 13:36:50 -0400

    mvp for sprint

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const app = render(<App />);
+    const header = app.getByText(/react plants/i)
+    expect(header).toBeInTheDocument();
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+    const { getByRole, getByLabelText, findAllByText } = render(<CheckoutForm />);
+
+    fireEvent.change(getByLabelText(/first name/i), {
+        target: { value: "Robert" }
+      });
+      fireEvent.change(getByLabelText(/last name/i), {
+        target: { value: "Gondola" }
+      });
+      fireEvent.change(getByLabelText(/address/i), {
+        target: { value: "My House Street" }
+      });
+      fireEvent.change(getByLabelText(/city/i), {
+        target: { value: "Charlotte" }
+      });
+      fireEvent.change(getByLabelText(/state/i), {
+        target: { value: "North Carolina" }
+      });
+      fireEvent.change(getByLabelText(/zip/i), {
+        target: { value: "28173" }
+      });
+
+    const button = getByRole('button')
+    fireEvent.click(button)
+    const formData = findAllByText(/robert/i)
+    expect(formData).toBeInTheDocument
+});
